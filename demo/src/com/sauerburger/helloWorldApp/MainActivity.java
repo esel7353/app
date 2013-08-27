733206013fa5b1f38d29c456542663f8461ce6cc
Frank Sauerburger <esel@gegensinn.org>
2013-08-27 22:30:37 +0200

    hey dude app

--- demo/src/com/sauerburger/helloWorldApp/MainActivity.java
+++ demo/src/com/sauerburger/helloWorldApp/MainActivity.java
@@ -10,6 +10,30 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TestView view = new TestView(this); 
+        setContentView(view);  // display my test view
     }
 }
+
+/**
+ * Simple test view, which should display "hey dude" and the number
+ * calls of the draw method.
+ */
+class TestView extends android.view.View {
+
+  android.graphics.Paint paint;
+  int calls = 0;
+
+  TestView (android.content.Context context) {
+    super(context);
+    paint = new android.graphics.Paint();
+    paint.setARGB(255, 255, 0, 0);
+  }
+
+  protected void onDraw(android.graphics.Canvas canvas) {
+    canvas.drawText("hey dude " + calls, 50, 50, paint);
+    calls++;
+  }
+
+}
+

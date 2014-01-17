9be59164f78269d18fa594ba0c1e4509888eed36
Saeuble <hans_mueller189@web.de>
2014-01-17 18:07:24 +0100

    Fehlerhaft

--- demo2/src/com/uhl/demo2/MainActivity.java
+++ demo2/src/com/uhl/demo2/MainActivity.java
@@ -2,14 +2,26 @@ package com.uhl.demo2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.content.Intent;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class MainActivity extends Activity{
+  public final static String EXTRA_MESSAGE = "com.saeuble.demo2.MESSAGE";
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+
+  public void sendMessage(View view) {
+    Intent intent = new Intent(this, DisplayMessageActivity.class);
+    EditText editText = (EditText) findViewById(R.id.edit_message);
+    String message = editText.getText().toString();
+    intent.putExtra(EXTRA_MESSAGE, message);
+    startActivity(intent);
+  }
 }

a60e424fda7c31c9207a08cb592bfd76fab94969
Saeuble <hans_mueller189@web.de>
2014-01-21 21:10:46 +0100

    Search word app (demo2)

--- demo2/src/com/uhl/demo2/DisplayMessageActivity.java
+++ demo2/src/com/uhl/demo2/DisplayMessageActivity.java
@@ -4,50 +4,35 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.MenuItem;
+import android.view.MenuInflater;
 import android.widget.TextView;
+import android.widget.EditText;
 import android.content.Intent;
-import android.support.v4.app.NavUtils;
+import android.annotation.SuppressLint;
 
-class DisplayMessageActivity extends Activity {
+public class DisplayMessageActivity extends Activity {
 
-//  @SuppressLint("NewApi")
-//  @Override
-//  protected void onCreate(Bundle savedInstanceState) {
-//    super.onCreate(savedInstanceState);
-//    setContentView(R.layout.activity_display_message);
-//
-//     Make sure we're running on Honeycomb or higher to use ActionBar APIs
-//    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-//       Show the Up button in the action bar.
-//      getActionBar().setDisplayHomeAsUpEnabled(true);
-//    }
-//  }
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-//  @SuppressLint("NewApi")
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-  
-    // Get the message from the intent
-    Intent intent = getIntent();
-    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-  
-    // Create the text view
-    TextView textView = new TextView(this);
-    textView.setTextSize(40);
-    textView.setText(message);
-  
-    // Set the text view as the activity layout
-    setContentView(textView);
-  }
+//         Make sure we're running on Honeycomb or higher to use ActionBar APIs
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+//             Show the Up button in the action bar.
+//            getActionBar().setDisplayHomeAsUpEnabled(true);
+//        }
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    switch (item.getItemId()) {
-    case android.R.id.home:
-      NavUtils.navigateUpFromSameTask(this);
-      return true;
+        TextView textView = new TextView(this);
+        textView.setText(message);
+
+        setContentView(textView);
     }
-    return super.onOptionsItemSelected(item);
-  }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 }
--- demo2/src/com/uhl/demo2/MainActivity.java
+++ demo2/src/com/uhl/demo2/MainActivity.java
@@ -3,25 +3,54 @@ package com.uhl.demo2;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.content.Intent;
 
-public class MainActivity extends Activity{
-  public final static String EXTRA_MESSAGE = "com.saeuble.demo2.MESSAGE";
-  /** Called when the activity is first created. */
-  @Override
-  public void onCreate(Bundle savedInstanceState)
-  {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
-  }
+public class MainActivity extends Activity
+    {
+    public final static String EXTRA_MESSAGE = "com.saeuble.demo2.MESSAGE";
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
 
-  public void sendMessage(View view) {
-    Intent intent = new Intent(this, DisplayMessageActivity.class);
-    EditText editText = (EditText) findViewById(R.id.edit_message);
-    String message = editText.getText().toString();
-    intent.putExtra(EXTRA_MESSAGE, message);
-    startActivity(intent);
-  }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.action_info:
+                Intent intent = new Intent(this, DisplayMessageActivity.class);
+                String message = getString(R.string.help);
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText1 = (EditText) findViewById(R.id.edit_message1);
+        EditText editText2 = (EditText) findViewById(R.id.edit_message2);
+        String word = editText1.getText().toString();
+        String text = editText2.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, text.replace(word, word));
+        startActivity(intent);
+    }
 }
